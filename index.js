396a639814f31a20b60d716eba6010e1c5f9d77c
Engr. Nahean <23103314@iubat.edu>
2026-07-16 12:29:05 +0600

    Add basic JavaScript code

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(`hello`);
+console.log(`i like pizza`);
+window.alert(`this is a alert`);
+window.alert(`i llike pizza`);
+
+/*
+multilin commets
+multilin ecommets
+multi
+*/
+
+document.getElementById("myH1").textContent=`hello`;
+document.getElementById("myP").textContent=`i love you`;
